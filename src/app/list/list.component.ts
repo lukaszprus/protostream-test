cfa28564179a5259b7dbbe51f92a7e17cec4afa7
Lukasz Prus <lucas.prus@gmail.com>
2023-04-28 22:40:14 +0200

    Async pipe in ListComponent

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 type ImageType = 'channel-badge' | 'thumbnail' | 'packshot' | 'hero-web' | 'hero-mobile';
 
@@ -23,33 +24,25 @@ type TransformedImages = { [T in ImageType]?: string; };
 
 export type TransformedItem = Omit<Item, "images"> & {
   images: TransformedImages;
-}
+};
 
 @Component({
   templateUrl: './list.component.html'
 })
-export class ListComponent implements OnInit, OnDestroy {
-  subs: Subscription | undefined;
-  items: TransformedItem[] | undefined;
+export class ListComponent {
+  items$: Observable<TransformedItem[]>;
 
-  constructor(private http: HttpClient) {}
+  constructor(http: HttpClient) {
+    this.items$ = http.get<Item[]>('https://content-cache.watchcorridor.com/v6/interview').pipe(
+      map(items => items.map(item => {
+        const images = item.images ? item.images.reduce((transformedImages, image) => {
+          transformedImages[image.type] = image.url;
 
-  ngOnInit() {
-    this.subs = this.http.get<Item[]>('https://content-cache.watchcorridor.com/v6/interview')
-      .subscribe(items => {
-        this.items = items.map(item => {
-          const images = item.images ? item.images.reduce((transformedImages, image) => {
-            transformedImages[image.type] = image.url;
+          return transformedImages;
+        }, {} as TransformedImages) : {};
 
-            return transformedImages;
-          }, {} as TransformedImages) : {};
-
-          return { ...item, images };
-        });
-      });
-  }
-
-  ngOnDestroy() {
-    this.subs && this.subs.unsubscribe();
+        return { ...item, images };
+      }))
+    );
   }
 }
